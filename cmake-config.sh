9d389d8b4bf3f7a91ce2720cec9be652ed4809a2
Rayhub <1102290321@qq.com>
2022-10-19 07:31:32 +0000

    default build change to RelWithDebInfo

--- cmake-config.sh
+++ cmake-config.sh
@@ -3,7 +3,7 @@
 cmake   -DLLVMCC=/usr/local/bin/clang \
         -DLLVMCXX=/usr/local/bin/clang++ \
         -DCMAKE_EXPORT_COMPILE_COMMANDS=1 \
-        -DCMAKE_BUILD_TYPE=Release \
+        -DCMAKE_BUILD_TYPE=RelWithDebInfo \
         -DENABLE_SOLVER_Z3=ON \
         -DLLVM_CONFIG_BINARY=/usr/local/bin/llvm-config \
         ../
