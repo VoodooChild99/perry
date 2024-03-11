8cb1436658fc550511df2959d1f660dec5a44a1b
Rayhub <1102290321@qq.com>
2024-03-11 09:34:06 +0000

    update build script

--- cmake-config.sh
+++ cmake-config.sh
@@ -1,32 +1,36 @@
 #!/bin/bash
 
+rm -rf build && mkdir build
+cd build || exit 1
+
+if [ -z $LLVM_CONFIG ]; then
+	echo "[*] LLVM_CONFIG is not specified, default to llvm-config"
+	LLVM_CONFIG=llvm-config
+fi
+
+LLVM_CONFIG_BIN=$(which $LLVM_CONFIG) || (echo "[x] Failed to locate $LLVM_CONFIG." && exit 1)
+
 if [ "$DEBUG" = "1" ]; then
-	cmake -DLLVMCC=/usr/local/bin/clang \
-				-DLLVMCXX=/usr/local/bin/clang++ \
-				-DCMAKE_EXPORT_COMPILE_COMMANDS=1 \
-				-DENABLE_SOLVER_Z3=ON \
-				-DENABLE_SOLVER_STP=OFF \
-				-DLLVM_CONFIG_BINARY=/usr/local/bin/llvm-config \
-				-DCMAKE_BUILD_TYPE=Debug \
-				../
+	cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=1 \
+		  -DENABLE_SOLVER_Z3=ON \
+		  -DENABLE_SOLVER_STP=OFF \
+		  -DLLVM_CONFIG_BINARY=$LLVM_CONFIG_BIN \
+		  -DCMAKE_BUILD_TYPE=Debug \
+		  ../
 elif [ "$ASAN" = "1" ]; then
-	cmake -DLLVMCC=/usr/local/bin/clang \
-				-DLLVMCXX=/usr/local/bin/clang++ \
-				-DCMAKE_EXPORT_COMPILE_COMMANDS=1 \
+	cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=1 \
 				-DENABLE_SOLVER_Z3=ON \
 				-DENABLE_SOLVER_STP=OFF \
-				-DLLVM_CONFIG_BINARY=/usr/local/bin/llvm-config \
+				-DLLVM_CONFIG_BINARY=$LLVM_CONFIG_BIN \
 				-DCMAKE_BUILD_TYPE=Debug \
 				-DCMAKE_C_FLAGS="-O0 -g -fsanitize=address -fno-omit-frame-pointer -fno-optimize-sibling-calls" \
 				-DCMAKE_CXX_FLAGS="-O0 -g -fsanitize=address -fno-omit-frame-pointer -fno-optimize-sibling-calls" \
 				../
 else
-	cmake -DLLVMCC=/usr/local/bin/clang \
-				-DLLVMCXX=/usr/local/bin/clang++ \
-				-DCMAKE_EXPORT_COMPILE_COMMANDS=1 \
+	cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=1 \
 				-DENABLE_SOLVER_Z3=ON \
 				-DENABLE_SOLVER_STP=OFF \
-				-DLLVM_CONFIG_BINARY=/usr/local/bin/llvm-config \
-				-DCMAKE_BUILD_TYPE=RelWithDebInfo \
+				-DLLVM_CONFIG_BINARY=$LLVM_CONFIG_BIN \
+				-DCMAKE_BUILD_TYPE=Release \
 				../
 fi
